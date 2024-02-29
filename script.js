82d933bac94313d19eb166d58bf0ed73b5b00a19
Mikołaj <128632183+TurboMiki@users.noreply.github.com>
2024-02-29 09:52:49 +0100

    Update

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+'use strict';
 class person{
     constructor(mass, height){
         this.mass = mass;
@@ -17,3 +18,35 @@ console.log('BMI: ${Mark}')
 
 // Loose (==) and strict(===) equality operator -> Pierwszy porównuje wartości z ukrytą konwersjątypów, tj '18' jest równwe 18, drugi porównuje także typy, tj. '18' nie jest równe 18
 
+/// Part 2 ofthe course : 'use strict'; -> strict mode, helps write secure code use on the very beginning of the script file
+
+//Basic function syntax: function NAME(PARAMITERS){LOGIC;}
+//Function expression syntax: const VARIABLE = function (PARAMITERS) {LOGIC} like so:
+const clacAge = function(birthYear){return 2037-birthYear;}
+const age = clacAge(2001);
+console.log(age)
+
+//Difference between declaration and expression is that expression cannot be used before declaration (hoisting)
+
+//Arrow funstion syntax: const VARIABLE = (PARAMITERS) => {LOGIC};  example:
+const yearsUntilReti = (birthyear) => {
+    const age1 = 2037-birthyear;
+    const retirement = 65 - age1;
+    return retirement;
+}
+
+/// Arrays:
+//array syntax: const ARRAYNAME = [VALUE1,VALUE2,VALUE3,...];
+//              const ARRAYNAME = new Array(1,2,3,4); 
+
+///Objects (dictionaries in Python):
+//object syntax: const OBJNAME = {VARIABLE1:VALUE1,VARIABLE2:VALUE2,...};
+//Positions can be accessed uding . notation -> object.variable1 OR
+//using key syntax -> object['variable1']
+//
+//One important thing -> Value in object's declaration doesn't have to be a value it can be a function, therefore creating a method
+// 
+
+///LOOPS:
+//for loop syntax: for(let COUNTER=0; COUNTER<=10; COUNTER++){LOGIC;} -składnia prawie identyczna do C
+//while loop syntax: same as ewerywhere -> while(condition){Logic;}
